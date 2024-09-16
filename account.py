0c2b9bdc8b291d21310e0142a248cd9ff6c0667e
EmilyGrigoryan <146064706+Emilia2004@users.noreply.github.com>
2024-09-16 14:14:11 +0400

    Add files via upload

--- account.py
+++ account.py
@@ -1,7 +1,7 @@
-from abc import ABC, abstractmethod
+from abc import ABC,abstractmethod
 class Account(ABC):
     
-    def __init__(self,account_number:int, balance: float, account_type:str ):
+    def __init__(self,account_number:int, balance:float, account_type:str ):
         self.__account_number = account_number
         self.__account_type = account_type
         self.__balance = balance
@@ -16,6 +16,8 @@ class Account(ABC):
     
     def getBalance(self):
         return self.__balance
+    
+
     
     
     def setAccountNumber(self,number):
@@ -163,3 +165,71 @@ class SavingsAccount(Account):
     def get_account_type(self):
         return self.__account_type
     
+
+class JointAccount(Account):
+    def __init__(self, account_number: int, balance: float, account_type:str,interest_rate:float, joint_owners: list[str]): 
+        super().__init__(account_number, balance, account_type,interest_rate)
+        self.__joint_owners = joint_owners
+
+    
+    def setJointOwners(self,owners):
+        if len(owners) >= 1:
+            self.__joint_owners = owners
+    
+
+    def getJointOwners(self):
+        return self.__joint_owners
+    
+
+
+    def add_owner(self, customer_name: str) -> None:
+        self.__joint_owners.append(customer_name)
+
+
+    def deposit(self,amount : float):
+        if amount >= 0:
+            print(f"Deposit of {amount}AMD is added successfully.You will have {self.__balance+amount*self.__interest__rate/100}AMD additionaly on your account")
+            self.__balance += amount*self.__interest__rate/100        
+        else:
+            print("Deposit can't be lower than 0")
+    
+    
+    def withdraw(self, amount: float):
+        if amount > 0 and amount <= self.__balance:
+            print(f"Withdrawal of {amount}AMD is available. You will have {self.__balance-amount*self.__interest__rate/100}AMD on your account")
+            self.__balance -= amount*self.__interest__rate/100
+        elif amount < 0:
+            print("Withdrawal can't ne lower than 0")
+        elif amount > self.__balance:
+            print("Insufficient funds")
+    
+    
+    def transfer(self, destination: Account, amount: float):
+        if amount > 0 and amount <= self.__balance:
+            print(f"Transer of {amount}AMD to {destination} is done successfully. You have {self.__balance-amount*self.__interest__rate/100}AMD on your account")
+            self.__balance -= amount*self.__interest__rate/100
+        elif amount < 0:
+            print(f"Transfer amount can't ne lower than 0")
+        elif amount > self.__balance:
+            print("Insufficient funds")
+
+        
+    def show_balance(self):
+        return self.__balance
+    
+    
+    def get_account_type(self):
+        return self.__account_type
+    
+
+
+user = CheckingAccount(123456,2500.0,"sdfgh",230.02)
+id = int(input("Enter Id"))
+user.setAccountNumber(id)
+
+
+
+
+
+
+
