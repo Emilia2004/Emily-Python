0184dd7fe300dc55b915e07c362dd65aaf9202c4
EmilyGrigoryan <146064706+Emilia2004@users.noreply.github.com>
2024-12-25 00:17:00 +0400

    Delete Task20.8.py

--- Task20.8.py
+++
@@ -1,9 +0,0 @@
-def update(**kwargs):
-    for i,j in kwargs.items():
-        print(f"{i} : {j}")
-
-def user(**kargs):
-    print("Updating settings!")
-    update(**kargs)
-
-user(name = "Bob",language = "Armenian",university = "YSU")
