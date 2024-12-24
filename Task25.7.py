286de3a353f7298470b074d7c1a73f1bd22ded6a
EmilyGrigoryan <146064706+Emilia2004@users.noreply.github.com>
2024-12-25 00:24:58 +0400

    Delete Task25.7.py

--- Task25.7.py
+++
@@ -1,9 +0,0 @@
-def custom_range(start, end, step):
-    size = int((end-start)/step)
-    for i in range(size+1):
-        yield start 
-        start += step
-
-gen = custom_range(0, 5, 0.5)
-for i in gen:
-    print(i)
