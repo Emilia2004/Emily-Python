b70d07585164e6fe914829e68b5491748fd4fde1
EmilyGrigoryan <146064706+Emilia2004@users.noreply.github.com>
2024-12-25 00:23:41 +0400

    Delete Task25.13.py

--- Task25.13.py
+++
@@ -1,9 +0,0 @@
-def custom_filter(func,iterable):
-    for item in iterable:
-        if func(item):
-            yield item
-
-gen = custom_filter(lambda x: x if x % 2 == 0 else None, [1,2,3,4,5,6,22,26])
-for i in gen:
-    print(i)
-
