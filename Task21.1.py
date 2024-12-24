eb9c8a36a1eef7b845b45a45317cf125f0750be7
EmilyGrigoryan <146064706+Emilia2004@users.noreply.github.com>
2024-12-25 00:17:11 +0400

    Delete Task21.1.py

--- Task21.1.py
+++
@@ -1,9 +0,0 @@
-def my_map(func,iterable):
-    result = []
-    for i in iterable:
-        result.append(func(i))
-    return result
-def double(item):
-    return item * 2
-
-print(my_map(double,[1,5,6,3,7]))
