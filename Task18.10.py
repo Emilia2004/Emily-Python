0ab560d9a39bbbed1c250aab05ce1599f2d00f90
EmilyGrigoryan <146064706+Emilia2004@users.noreply.github.com>
2024-12-25 00:11:09 +0400

    Delete Task18.10.py

--- Task18.10.py
+++
@@ -1,8 +0,0 @@
-word = input("Enter word: ")
-def polindrome(st):
-    if not st:
-        return True
-    if st[0] != st[len(st)-1]:
-        return False
-    return polindrome(st[1:-1])
-print(polindrome(word))
