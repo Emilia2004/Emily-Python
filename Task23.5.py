a7ce20a13c729bd3d09fc03e523d72c4fc0204de
EmilyGrigoryan <146064706+Emilia2004@users.noreply.github.com>
2024-12-25 00:21:56 +0400

    Delete Task23.5.py

--- Task23.5.py
+++
@@ -1,8 +0,0 @@
-def power_factory(n):
-    def raising(i):
-        return n ** i
-    return raising
-num = int(input("Enter number: "))
-power = int(input("Enter power: "))
-func = power_factory(num)
-print(f"The {power} power of the number {num} is {func(power)}")
