f613157bbd7eb151a7c0312d67c982c9b6c0d328
EmilyGrigoryan <146064706+Emilia2004@users.noreply.github.com>
2024-12-25 00:16:23 +0400

    Delete Task20.5.py

--- Task20.5.py
+++
@@ -1,8 +0,0 @@
-product = {
-        "type" : "car",
-        "name" : "mercedes",
-        "colour" : "black",
-        "birth" : 2008 }
-def display_product(*,type,name,colour,birth):
-    print(type,name,colour,birth)
-display_product(**product)
