ac4499c85225534b5b040a2bb05f7b56c010b32a
EmilyGrigoryan <146064706+Emilia2004@users.noreply.github.com>
2024-12-25 00:22:34 +0400

    Delete Task23.8.py

--- Task23.8.py
+++
@@ -1,9 +0,0 @@
-def make_greeting(greeting):
-    def greet(name):
-        print(greeting,name)
-    return greet
-
-greeting = input("Enter greeting message: ")
-name = input("Enter user's name: ")
-func = make_greeting(greeting)
-func(name)
