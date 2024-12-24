781c2ec4c1bdbe8e317576fd92bd5f07e8e1dbd3
EmilyGrigoryan <146064706+Emilia2004@users.noreply.github.com>
2024-12-25 00:09:09 +0400

    Delete Task17.3.py

--- Task17.3.py
+++
@@ -1,8 +0,0 @@
-def listfunc(ls):
-    for i in ls:
-        print(i,end = " ")
-size = int(input("Enter size of list: "))
-ls = []
-for i in range(size):
-    ls.append(int(input("Enter element: ")))
-listfunc(ls)
