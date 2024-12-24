19c0e9883ac7194685446e3b9ff5b41237aa7371
EmilyGrigoryan <146064706+Emilia2004@users.noreply.github.com>
2024-12-25 00:15:17 +0400

    Delete Task20.1.py

--- Task20.1.py
+++
@@ -1,6 +0,0 @@
-name = input("Enter name: ")
-lastname = input("Enter lastname: ")
-def greeting(name1,name2,greet = "Welcome"):
-    print(greet,name1,name2)
-greeting(name,lastname)
-greeting(name,lastname,"Hello")
